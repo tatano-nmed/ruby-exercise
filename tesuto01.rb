7abe99e910dee48674af201c8a97186e35cdf1af
setsuko tatano <setsuko.tatano@nacl-med.co.jp>
2020-08-24 14:42:35 +0900

    ::# Please enter the commit message for your changes. Lines starting

---
+++ tesuto01.rb
@@ -0,0 +1,55 @@
+print("Hello, Ruby.\n")
+
+puts "aaaa,Ruby."
+puts "aaaa", "Ruby."
+
+puts Math.sin(4)  #関数
+
+x = 10
+y = 20
+z = 30
+area = (x*y + y*z + z*x) * 2
+puts "表面積=#{area}"
+
+#配列
+
+names = ["aa","bb","cccccc"]
+puts names.size
+
+names.each do |n|
+ puts n
+end
+
+#Hash
+
+address = {name:"高橋" , kananame:"タカハシ" , tel:"010-1113-4444"}
+address.each do |key,value|
+  puts "#{key}: #{value}"
+end
+
+
+#代入 *項目は１つのみ配列となる
+
+first, second,*rast = 1,2,3,4,5
+p [first , second,rast]
+first, *second,rast = 1,2,3,4,5
+p [first , second , rast]
+first, second,rast = 1,2,3,4,5
+p [first , second , rast]
+first, second,*rast = 1,2,3
+p [first , second , rast]
+
+#
+ary1 = []
+ary2 = []
+p ary1.object_id
+p ary2.object_id
+
+#
+
+str11 = "foo"
+str12 = str11
+str13 = "f" + "o" + "o"
+p str11 == str12
+p str11 == str13
+
